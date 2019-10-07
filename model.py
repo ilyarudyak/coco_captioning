3b263864f8dc9d7392729b0df4c7cf840d21196f
Ilya Rudyak <ilyarudyak@yahoo.com>
2019-10-07 14:16:01 +0300

    added decoder

--- model.py
+++ model.py
@@ -23,11 +23,32 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super().__init__()
+
+        self.embedding = nn.Embedding(num_embeddings=vocab_size,
+                                      embedding_dim=embed_size)
+
+        self.lstm = nn.LSTM(input_size=embed_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True)
+
+        self.linear = nn.Linear(in_features=hidden_size,
+                                out_features=vocab_size)
     
     def forward(self, features, captions):
-        pass
+
+        captions = captions[:, :-1]
+        captions_embed = self.embedding(captions)
+        captions_embed = torch.cat((features.unsqueeze(1), captions_embed), dim=1)
+
+        lstm_out, _ = self.lstm(captions_embed)
+        scores = self.linear(lstm_out)
+
+        return scores
 
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        """ Accepts pre-processed image tensor (inputs) and returns
+            predicted sentence (list of tensor ids of length max_len)
+        """
         pass
